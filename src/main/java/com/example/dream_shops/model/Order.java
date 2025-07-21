63d56142354502a4e6368098dc43f0bdb7c6d7f5
Smim7 <somayamorshed7@gmail.com>
2025-07-21 19:10:42 -0400

    add fields to entity class order and OrderItem

--- src/main/java/com/example/dream_shops/model/Order.java
+++ src/main/java/com/example/dream_shops/model/Order.java
@@ -1,5 +1,30 @@
 package com.example.dream_shops.model;
 
+import com.example.dream_shops.enums.OrderStatus;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
 public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private LocalDate orderDate;
+
+    @Enumerated(EnumType.STRING)
+    private OrderStatus orderStatus;
+
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL,orphanRemoval = true)
+    private Set<OrderItem> orderItems=new HashSet<>();
 
 }
