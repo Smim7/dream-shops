5fac46b03589de3bb59b348025d33486b372f366
Smim7 <somayamorshed7@gmail.com>
2025-07-23 02:51:09 -0400

    created AuthTokenFilter,JwtAuthEntryPoint

--- src/main/java/com/example/dream_shops/security/confiq/ShopConfiq.java
+++ src/main/java/com/example/dream_shops/security/confiq/ShopConfiq.java
@@ -1,13 +1,69 @@
 package com.example.dream_shops.security.confiq;
 
+import com.example.dream_shops.security.jwt.AuthTokenFilter;
+import com.example.dream_shops.security.jwt.JwtAuthEntryPoint;
+import com.example.dream_shops.security.user.ShopUserDetailsService;
+import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+import java.util.List;
+
+@RequiredArgsConstructor
+@EnableWebSecurity
 @Configuration
 public class ShopConfiq {
-    @Bean
+    private final ShopUserDetailsService UserDetailsService;
+    private final JwtAuthEntryPoint authEntryPoint;
+    private static final List<String> SECURED_URLS=List.of(e1:"");
+
+
+
+@Bean
     public ModelMapper modelMapper(){
         return new ModelMapper();
+    }
+@Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+@Bean
+    public AuthTokenFilter authTokenFilter(){
+        return new AuthTokenFilter();
+    }
+@Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration)throws Exception{
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+@Bean
+    public DaoAuthenticationProvider daoAuthenticationProvider(){
+     var authProvider = new DaoAuthenticationProvider();
+     authProvider.setUserDetailsService(UserDetailsService);
+     authProvider.setPasswordEncoder(passwordEncoder());
+     return authProvider;
+    }
+@Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+        http.csrf(AbstractHttpConfigurer::disable)
+                .exceptionHandling(exception -> exception.authenticationEntryPoint(authEntryPoint))
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(auth ->auth.requestMatchers(SECURED_URLS.toArray(String[]::new)).authenticated()
+                        .anyRequest().permitAll());
+        http.authenticationProvider(daoAuthenticationProvider());
+        http.addFilterBefore(authTokenFilter(), UsernamePasswordAuthenticationFilter.class);
+        return http.build();
     }
 }
